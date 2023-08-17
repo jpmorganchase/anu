aa714192d3ccccf30e9c2339b859055eec772624
David Saffo <david.saffo@outlook.com>
2023-08-17 16:25:34 -0400

    fixed bind instance and added top level version

--- src/bind.ts
+++ src/bind.ts
@@ -26,24 +26,24 @@ export function bind(shape: string, scene: Scene, options: object = {}, data: Ar
   return new Selection(meshes, scene);
 }
 
-// /**
-//  * Take a selection, a shape type, and data. For each index in the data create a new mesh for each node in the selection as the parrent.
-//  * The data index of the mesh is also attached to the mesh node object under the metadate property.
-//  *
-//  * @param mesh The mesh to create instances from.
-//  * @param data The data to bind elements too, must be passed as a list of objects where each object represents a row of tabular data.
-//  * @returns An instance of Selection, a class contating a array of selected nodes, the scene, and the functions of the class Selection,
-//  * or undefined if a selection could not be made.
-//  */
-// export function bindInstance(mesh: Mesh, scene: Scene, data: Array<object> = [{}]): Selection {
-//   let meshes: Node[] = [];
-//     data.forEach((element, i) => {
-//       var instance = mesh.createInstance(mesh.name + '_' + i, scene);
-//       if (mesh instanceof InstancedMesh) mesh.actionManager = new ActionManager(scene);
-//       Tags.EnableFor(instance);
-//       instance.metadata = { ...mesh.metadata, data: element };
-//       meshes.push(instance as InstancedMesh);
-//     });
-
-//   return new Selection(meshes, scene);
-// }
+/**
+ * Take a selection, a shape type, and data. For each index in the data create a new mesh for each node in the selection as the parrent.
+ * The data index of the mesh is also attached to the mesh node object under the metadate property.
+ *
+ * @param mesh The mesh to create instances from.
+ * @param data The data to bind elements too, must be passed as a list of objects where each object represents a row of tabular data.
+ * @returns An instance of Selection, a class contating a array of selected nodes, the scene, and the functions of the class Selection,
+ * or undefined if a selection could not be made.
+ */
+export function bindInstance(mesh: Mesh, scene: Scene, data: Array<object> = [{}]): Selection {
+  let meshes: Node[] = [];
+  data.forEach((element, i) => {
+    var instance = mesh.createInstance(mesh.name + '_' + i);
+    if (mesh instanceof InstancedMesh) mesh.actionManager = new ActionManager(scene);
+    Tags.EnableFor(instance);
+    instance.metadata = { ...mesh.metadata, data: element };
+    meshes.push(instance as InstancedMesh);
+  });
+  
+  return new Selection(meshes, scene);
+}
