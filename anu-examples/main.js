c5416151934f14fc01d89bc3cedf81e1525c058e
Feiyu <ericlu-arvr@outlook.com>
2024-04-30 16:02:10 -0400

    added smallmultiples example

--- anu-examples/main.js
+++ anu-examples/main.js
@@ -22,6 +22,7 @@ import { linechart2D } from './examples/LineCharts/linechart2D';
 import { textureMap } from './examples/Geographic/Texture_Map';
 import { textureGlobe } from './examples/Geographic/Texture_Globe';
 import { layout } from './examples/Layouts/layout';
+import { smallMultiples } from './examples/Layouts/smallMultiples';
 import { cotBind } from './examples/Selections/cot_bind';
 import { spheresBind } from './examples/Selections/spheres_bind';
 import { cotTransform } from './examples/Selections/cot_transform';
@@ -78,6 +79,7 @@ const scenes = {
   'textureMap': textureMap,
   'textureGlobe': textureGlobe,
   'layout': layout,
+  'smallMultiples': smallMultiples,
   'cot_bind': cotBind,
   'spheres_bind': spheresBind,
   'cot_transform': cotTransform,
