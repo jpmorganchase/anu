0a15ed9b2631ba2246f697e7e44b5a0e35a6987d
Saffo, David <david.saffo@jpmchase.com>
2025-03-03 14:06:44 -0500

    tutorial content

--- docs/clean_ext.js
+++ docs/clean_ext.js
@@ -1,10 +1,10 @@
 import fs from "fs";
-import path from "path"
+import path from "path";
 
 // Directory to search
 const directory = './public/tutorial';
 
-// Function to rename files
+// Function to rename files and update index.html
 function renameFiles(dir) {
     fs.readdir(dir, (err, files) => {
         if (err) {
@@ -31,6 +31,30 @@ function renameFiles(dir) {
                             console.error(`Error renaming file: ${err}`);
                         } else {
                             console.log(`Renamed: ${filePath} -> ${newFilePath}`);
+                        }
+                    });
+                } else if (file === 'index.html') {
+                    // Add script tag to index.html
+                    fs.readFile(filePath, 'utf8', (err, data) => {
+                        if (err) {
+                            console.error(`Error reading index.html: ${err}`);
+                            return;
+                        }
+
+                        // Check if the script tag already exists
+                        if (!data.includes('<script src="coi-serviceworker.min.js"></script>')) {
+                            const updatedData = data.replace(
+                                /<\/body>/i,
+                                '    <script src="coi-serviceworker.min.js"></script>\n</body>'
+                            );
+
+                            fs.writeFile(filePath, updatedData, 'utf8', err => {
+                                if (err) {
+                                    console.error(`Error writing to index.html: ${err}`);
+                                } else {
+                                    console.log(`Updated index.html with script tag.`);
+                                }
+                            });
                         }
                     });
                 }
